63b51c89d430daddbe65efd11e1b19b460680f95
Raveendra Soori <sooriravindra@gmail.com>
2022-02-19 08:49:28 -0500

    Add clang-format as a cmake target

--- src/app.cpp
+++ src/app.cpp
@@ -4,55 +4,48 @@ enum { ID_New = 1 };
 
 wxIMPLEMENT_APP(UnwordlerApp);
 
-bool UnwordlerApp::OnInit()
-{
-    wxFrame *frame = new MyFrame();
-    frame->Show(true);
-    return true;
+bool UnwordlerApp::OnInit() {
+  wxFrame *frame = new MyFrame();
+  frame->Show(true);
+  return true;
 }
 
 void MyFrame::InitMenubar() {
-    // File item
-    wxMenu *menuItemFile = new wxMenu;
-    menuItemFile->Append(ID_New, "&New...\tCtrl-N", "New Wordle solver");
-    menuItemFile->AppendSeparator();
-    menuItemFile->Append(wxID_EXIT);
+  // File item
+  wxMenu *menuItemFile = new wxMenu;
+  menuItemFile->Append(ID_New, "&New...\tCtrl-N", "New Wordle solver");
+  menuItemFile->AppendSeparator();
+  menuItemFile->Append(wxID_EXIT);
 
-    // Help item
-    wxMenu *menuItemHelp = new wxMenu;
-    menuItemHelp->Append(wxID_ABOUT);
-    wxMenuBar *menuBar = new wxMenuBar;
+  // Help item
+  wxMenu *menuItemHelp = new wxMenu;
+  menuItemHelp->Append(wxID_ABOUT);
+  wxMenuBar *menuBar = new wxMenuBar;
 
-    menuBar->Append(menuItemFile, "&File");
-    menuBar->Append(menuItemHelp, "&Help");
-    SetMenuBar(menuBar);
+  menuBar->Append(menuItemFile, "&File");
+  menuBar->Append(menuItemHelp, "&Help");
+  SetMenuBar(menuBar);
 
-    Bind(wxEVT_MENU, &MyFrame::OnNew, this, ID_New);
-    Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
-    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+  Bind(wxEVT_MENU, &MyFrame::OnNew, this, ID_New);
+  Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
+  Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
 }
 
 MyFrame::MyFrame()
-    : wxFrame(NULL, wxID_ANY, "Unwordler", wxDefaultPosition, wxSize(400,300)),
-    grid_{std::make_unique<MyGrid>(this, 5, 5)}
-{
-    MyFrame::InitMenubar();
-    grid_->Show(false);
-    grid_->Show(true);
+    : wxFrame(NULL, wxID_ANY, "Unwordler", wxDefaultPosition, wxSize(400, 300)),
+      grid_{std::make_unique<MyGrid>(this, 5, 5)} {
+  MyFrame::InitMenubar();
+  grid_->Show(false);
+  grid_->Show(true);
 }
 
-void MyFrame::OnExit(wxCommandEvent& event)
-{
-    Close(true);
+void MyFrame::OnExit(wxCommandEvent &event) { Close(true); }
+
+void MyFrame::OnAbout(wxCommandEvent &event) {
+  wxMessageBox("This is a Wordle solver app", "About Unwordler",
+               wxOK | wxICON_INFORMATION);
 }
 
-void MyFrame::OnAbout(wxCommandEvent& event)
-{
-    wxMessageBox("This is a Wordle solver app",
-            "About Unwordler", wxOK | wxICON_INFORMATION);
-}
-
-void MyFrame::OnNew(wxCommandEvent& event)
-{
-    wxLogMessage("Starting new solver");
+void MyFrame::OnNew(wxCommandEvent &event) {
+  wxLogMessage("Starting new solver");
 }
--- src/include/app.h
+++ src/include/app.h
@@ -8,34 +8,33 @@
 
 class MyGrid;
 
-class UnwordlerApp : public wxApp
-{
-    public:
-        virtual bool OnInit();
+class UnwordlerApp : public wxApp {
+ public:
+  virtual bool OnInit();
 };
 
-class MyFrame : public wxFrame
-{
-    public:
-        MyFrame();
-    private:
-        void OnNew(wxCommandEvent& event);
-        void OnExit(wxCommandEvent& event);
-        void OnAbout(wxCommandEvent& event);
-        void InitMenubar();
-        std::unique_ptr<MyGrid> grid_;
+class MyFrame : public wxFrame {
+ public:
+  MyFrame();
+
+ private:
+  void OnNew(wxCommandEvent& event);
+  void OnExit(wxCommandEvent& event);
+  void OnAbout(wxCommandEvent& event);
+  void InitMenubar();
+  std::unique_ptr<MyGrid> grid_;
 };
 
-class MyGrid
-{
-    public:
-        MyGrid(MyFrame * F, int rows, int columns);
-        void Show(bool);
-        bool SetRow(int, std::string);
-    private:
-        MyFrame * frame_;
-        std::vector<wxButton*> gridButtons_;
-        wxButton* goButton_;
-        int rows_;
-        int columns_;
+class MyGrid {
+ public:
+  MyGrid(MyFrame* F, int rows, int columns);
+  void Show(bool);
+  bool SetRow(int, std::string);
+
+ private:
+  MyFrame* frame_;
+  std::vector<wxButton*> gridButtons_;
+  wxButton* goButton_;
+  int rows_;
+  int columns_;
 };
